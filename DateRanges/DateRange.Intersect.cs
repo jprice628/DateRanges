6b9c829ede16777f1184d63b9d82505bc58587a6
jprice628 <jprice628@hotmail.com>
2019-04-17 22:19:00 -0400

    Implemented simple DateRange.Intersect(...) method.

---
+++ DateRanges/DateRange.Intersect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateRanges
+{
+    public partial struct DateRange
+    {
+        public IEnumerable<DateRange> Intersect(DateRange value)
+        {
+            if (IsEmpty() || value.IsEmpty())
+            {
+                return Enumerable.Empty<DateRange>();
+            }
+            else if (EndDate <= value.StartDate || value.EndDate <= StartDate)
+            {
+                return Enumerable.Empty<DateRange>();
+            }
+            else
+            {
+                return new[]
+                {
+                    new DateRange(
+                        Date.Max(StartDate, value.StartDate),
+                        Date.Min(EndDate, value.EndDate)
+                        )
+                };
+            }
+        }
+    }
+}
---
+++ Tests/DateRangeIntersectTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateRanges;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class DateRangeIntersectTests
+    {
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsEmptyWhenDr1IsEmpty()
+        {
+            // Arrange
+            var dr1 = DateRange.Empty();
+            var dr2 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 12, 31));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.IsFalse(intersection.Any());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsEmptyWhenDr2IsEmpty()
+        {
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 12, 31)); 
+            var dr2 = DateRange.Empty();
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.IsFalse(intersection.Any());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsEmptyWhenDr1LessThanDr2()
+        {
+            // Scenario:
+            // dr1    |-------|
+            // dr2            |------------|
+            // Result [Empty]
+            //
+            // Note: StartDates are inclusive. EndDates are exclusive.
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 6, 1));
+            var dr2 = new DateRange(Date.NewDate(2019, 6, 1), Date.NewDate(2019, 12, 31));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.IsFalse(intersection.Any());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsEmptyWhenDr2LessThanDr1()
+        {
+            // Scenario:
+            // dr1              |-------|
+            // dr2    |---------|
+            // Result [Empty]
+            //
+            // Note: StartDates are inclusive. EndDates are exclusive.
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 6, 1), Date.NewDate(2019, 12, 31));
+            var dr2 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 6, 1));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.IsFalse(intersection.Any());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsWhenDr1ContainsDr2()
+        {
+            // Scenario:
+            // dr1  |----------------|
+            // dr2      |---------|
+            // Result   |---------|
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 12, 31));
+            var dr2 = new DateRange(Date.NewDate(2019, 3, 1), Date.NewDate(2019, 9, 1));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.AreEqual(1, intersection.Count());
+            Assert.AreEqual(dr2, intersection.First());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsWhenDr2ContainsDr1()
+        {
+            // Scenario:
+            // dr1      |--------|
+            // dr2    |-------------|
+            // Result   |--------|
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 3, 1), Date.NewDate(2019, 9, 1));
+            var dr2 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 12, 31));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.AreEqual(1, intersection.Count());
+            Assert.AreEqual(dr1, intersection.First());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsWhenDr1LeftIntersectsDr2()
+        {
+            // Scenario:
+            // dr1      |--------|
+            // dr2         |---------|
+            // Result      |-----|
+            //
+            // Note: StartDates are inclusive. EndDates are exclusive.
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 1, 1), Date.NewDate(2019, 6, 1));
+            var dr2 = new DateRange(Date.NewDate(2019, 3, 1), Date.NewDate(2019, 9, 1));
+            var expected = new DateRange(Date.NewDate(2019, 3, 1), Date.NewDate(2019, 6, 1));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.AreEqual(1, intersection.Count());
+            Assert.AreEqual(expected, intersection.First());
+        }
+
+        [TestMethod]
+        public void DateRange_Intersect_ReturnsWhenDr1RightIntersectsDr2()
+        {
+            // Scenario:
+            // dr1          |--------|
+            // dr2      |---------|
+            // Result       |-----|
+            //
+            // Note: StartDates are inclusive. EndDates are exclusive.
+
+            // Arrange
+            var dr1 = new DateRange(Date.NewDate(2019, 6, 1), Date.NewDate(2019, 12, 1));
+            var dr2 = new DateRange(Date.NewDate(2019, 3, 1), Date.NewDate(2019, 9, 1));
+            var expected = new DateRange(Date.NewDate(2019, 6, 1), Date.NewDate(2019, 9, 1));
+
+            // Act
+            var intersection = dr1.Intersect(dr2);
+
+            // Assert
+            Assert.AreEqual(1, intersection.Count());
+            Assert.AreEqual(expected, intersection.First());
+        }
+    }
+}
